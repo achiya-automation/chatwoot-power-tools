af24b609b857181d29aaaa9237361b261364d4f8
Achiya Automation <info@achiya-automation.com>
2026-07-27 16:03:40 +0300

    build: bundle עם טאב נקרא/מקליד

--- modules/sequences/webapp/dist/assets/JourneyEditor-B1x8Iga1-20260727130326.js
+++ modules/sequences/webapp/dist/assets/JourneyEditor-B1x8Iga1-20260727130326.js
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:modules/sequences/webapp/dist/assets/JourneyEditor-DTjBSExi-20260727113650.js
-import{c as Re,r as Xr,g as Xa,R as Ua,a as Ga,b as k,j as g,u as ue,S as Ue,I as ce,B as Oe,T as Ur,L as it,d as Gr,e as Zt,f as Ka,P as Kr,n as Za,M as Io,X as Qa,h as Ja,U as el,C as tl,i as nl,A as ol,k as Zr,l as rl,m as sl,o as il,p as al,q as ll,s as cl,t as ul,v as dl,w as fl,x as hl,y as gl,z as pl,D as ml,E as xl,F as yl,G as vl,H as jo,J as wl,K as bl,N as _l,O as Tn,Q as Nl,V as $o}from"./main-ByPx2McP-20260727113650.js";import"./videoCompressRunner-DrfbSpyU-20260727113650.js";/**
-========
-import{c as Re,r as Xr,g as Xa,R as Ua,a as Ga,b as k,j as g,u as ue,S as Ue,I as ce,B as Oe,T as Ur,L as it,d as Gr,e as Zt,f as Ka,P as Kr,n as Za,M as Io,X as Qa,h as Ja,U as el,C as tl,i as nl,k as Zr,l as ol,m as rl,o as sl,p as il,q as al,s as ll,t as cl,A as ul,v as dl,w as fl,x as hl,y as gl,z as pl,D as ml,E as xl,F as yl,G as jo,H as vl,J as wl,K as bl,N as Tn,O as _l,Q as $o}from"./main-DFYn-umr-20260727121030.js";import"./videoCompressRunner-ChtLZHup-20260727121030.js";/**
->>>>>>>> origin/main:modules/sequences/webapp/dist/assets/JourneyEditor-BdTGZQIG-20260727121030.js
+import{c as Re,r as Xr,g as Xa,R as Ua,a as Ga,b as k,j as g,u as ue,S as Ue,I as ce,B as Oe,T as Ur,L as it,d as Gr,e as Zt,f as Ka,P as Kr,n as Za,M as Io,X as Qa,h as Ja,U as el,C as tl,i as nl,A as ol,k as Zr,l as rl,m as sl,o as il,p as al,q as ll,s as cl,t as ul,v as dl,w as fl,x as hl,y as gl,z as pl,D as ml,E as xl,F as yl,G as vl,H as jo,J as wl,K as bl,N as _l,O as Tn,Q as Nl,V as $o}from"./main-XDA9jjfH-20260727130326.js";import"./videoCompressRunner-ClL22XKC-20260727130326.js";/**
  * @license lucide-react v0.454.0 - ISC
  *
  * This source code is licensed under the ISC license.
